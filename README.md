acfa6a1a3e490d0895e24c48066bea7120179fde
方利 <fangli@autohome.com.cn>
2021-04-15 00:46:54 +0800

    解析html

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 汇丰银行汇率爬虫
 
 
-
+pip install -r requirements.txt
 
 crontab –e 
 0 */1 * * * sh /usr/local/txspider/start_role.sh >> /var/log/spider.log 2>&1
--- hsbc_spider.py
+++ hsbc_spider.py
@@ -2,9 +2,7 @@
 # __author__ : funny
 # __create_time__ : 16/11/6 10:41
 import requests
-import pymysql
-import config
-
+import bs4
 
 class ExchangeRateSpider(object):
     def craw(self):
@@ -16,36 +14,18 @@ class ExchangeRateSpider(object):
     def add_rate(self, rates):
         if rates is None or len(rates) == 0:
             return
-        try:
-            connection = pymysql.connect(**config.dbconfig)
-            with connection.cursor() as cursor:
-                sql = 'INSERT INTO cbg_role (yn,create_time'
-                for key, value in role.items():
-                    sql = sql + ',' + key
-                sql += ' ) values (1,now()'
-                for key, value in role.items():
-                    if type(value) == int:
-                        sql = sql + ',' + str(value)
-                    else:
-                        sql = sql + ',\'' + str(value.encode('utf-8').decode("utf-8")) + '\''
-                sql += ')'
-                self.log.logger.info(sql)
-                cursor.execute(sql)
-                connection.commit()
-        except Exception as e:
-            print(e)
-            pass
-        finally:
-            connection.close()
+        print(rates)
 
     def download(self, url):
         requests.adapters.DEFAULT_RETRIES = 3
         response = requests.get(url, timeout=3)
+        response.encoding = 'utf-8'
         if response.status_code != 200:
             return None
-        return response.text
+        return response.content.decode("utf-8")
 
     def parse(self, html_cont):
+        print(html_cont)
         if html_cont is None:
             return
         data = []
@@ -55,3 +35,6 @@ class ExchangeRateSpider(object):
 def rate_job():
     obj_spider = ExchangeRateSpider()
     obj_spider.craw()
+
+
+rate_job()
